074aa2fb237b304ac458a89a61f28ff80a77971e
M-H <marc-henri1@live.fr>
2017-03-11 15:21:20 +0100

    ajout methode add de classe listeTriee

--- tp2/src/HeritageEtInterfaces.java
+++ tp2/src/HeritageEtInterfaces.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Collections;
 /**
  *
  * @author M-H
@@ -8,9 +10,11 @@ public class HeritageEtInterfaces {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        CreneauHoraire c1 = new CreneauHoraire(87,10,00,1);
+        CreneauHoraire c1 = new CreneauHoraire(87,10,00,2);
         CreneauHoraireComparable c2 = new CreneauHoraireComparable(87,10,00,1);
+        CreneauHoraireComparable c4 = new CreneauHoraireComparable(87,10,00,3);
         CreneauHoraire c3 = (CreneauHoraire) c1.clone();
+        ListeTriee liste = new ListeTriee(); 
         
        boolean b = c1==c2; //compare les adresses memoire
        boolean a = c1.equals(c2); //compare aussi les adresses memoire mais a été redefinie dans la classe CreneauHoraire
@@ -21,6 +25,15 @@ public class HeritageEtInterfaces {
        /*c1.saisieCreneau();*/
        int valcomparaison = c2.compareTo(c1);
        System.out.println(valcomparaison);
+       if(liste.add(c2) || liste.add(c4)){
+           System.out.println("ajout reussi");
+       }
+       System.out.println(liste.get(0).getClass());
+       /*Collections.sort(liste);
+       int i;
+       for(i=0;i<=liste.size();i++){
+           System.out.println(liste.get(i).duree);
+       }*/
     }
     
 }
--- tp2/src/ListeTriee.java
+++ tp2/src/ListeTriee.java
@@ -1,8 +1,41 @@
 import java.util.ArrayList;
+import java.util.Collections;
 /**
  *
  * @author M-H
  */
-public class ListeTriee extends ArrayList {
+public class ListeTriee extends ArrayList implements Comparable{
     
+    //public CreneauHoraireComparable compa;
+    public ListeTriee(){
+        super();
+    }
+    
+    
+    @Override
+    public boolean add(Object o){//redefinition de la methode add pour pouvoir prendre que des elements comparable
+        if(!(o instanceof Comparable)){
+            return false;
+        }else{
+            super.add(o);
+            return true;
+        }
+    }
+    
+    @Override
+    public int compareTo(Object o){//redefinition de comparable pour eviter que liste triee soit une classe abstraite
+        
+        CreneauHoraireComparable compa = (CreneauHoraireComparable)o;
+        
+        return compa.compareTo(o);//utilisation de la methode de comparaison de creneauHoraireComparable
+    }
+    
+    
+    
+    public void sort(ListeTriee liste){
+        
+        //il faut preciser quelque part dans l'implantation de la classe que la liste prend des elements comparables et non des objets tout simples
+        
+        
+    }
 }
